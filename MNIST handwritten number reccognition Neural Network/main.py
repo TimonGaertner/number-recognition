bbe8691840216f20a5ecbda727718dc30608399d
1NO0B <noob@users.noreply.github.com>
2020-11-17 23:37:50 +0100

    Not optimized, but fully functional Neural Network

--- MNIST handwritten number reccognition Neural Network/main.py
+++ MNIST handwritten number reccognition Neural Network/main.py
@@ -1,42 +1,124 @@
+from __future__ import print_function
+import argparse
 import torch
 import torch.nn as nn
-import torch.nn.functional as func
+import torch.nn.functional as F
 import torch.optim as optim
+import torchvision
+from torch import classes
 from torchvision import datasets, transforms
-from torch.autograd import Variable
+from torch.optim.lr_scheduler import StepLR
+import matplotlib.pyplot as plt
+import numpy as np
+
+showimages=False
+epochs = 2
+
+kwargs = {'num_workers':0, 'pin_memory': True}
+
+trainloader = torch.utils.data.DataLoader(datasets.MNIST('/data', train=True, download=True,
+                                                                  transform=transforms.Compose([
+                                                                      transforms.ToTensor(),
+                                                                      transforms.Normalize((0.1307,), (0.3081,))
+                                                                  ])), batch_size=80, shuffle = True, **kwargs)
+
+testloader = torch.utils.data.DataLoader(datasets.MNIST('/data', train=False,
+                                                                  transform=transforms.Compose([
+                                                                      transforms.ToTensor(),
+                                                                      transforms.Normalize((0.1307,), (0.3081,))
+                                                                  ])), batch_size=80, shuffle = True, **kwargs)
 
 
 
 
-kwargs = {'num_workers':1, 'pin_memory': True}
-traindata = torch.utils.data.DataLoader(datasets.MNIST('data', train = True, download = True, transform = transforms.Compose([transforms.ToTensor(),
-                                                      transforms.Normalize((0.1307,),(0.3081,))])), batch_size = 48, shuffle = True, **kwargs)
-testdata = torch.utils.data.DataLoader(datasets.MNIST('data', train = False, transform = transforms.Compose([transforms.ToTensor(),
-                                                      transforms.Normalize((0.1307,),(0.3081,))])), batch_size = 48, shuffle = True, **kwargs)
+
+# imshow and the if statement under to show an example image of the dataset
+
+def imshow(img):
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+if showimages == True:
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    imshow(torchvision.utils.make_grid(images))
+
+
+
 class Net(nn.Module):
     def __init__(self):
-        pass
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=4)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=4)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(2420, 784)
+        self.fc2 = nn.Linear(784, 10)
 
     def forward(self, x):
-        pass
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
-model = Net()
-model.cuda()
 
-optimizer = optim.SGD(model.parameters(), lr = 0.1, momentum = 0.7)
+net = Net()
+net.cuda()
 
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+#train
 def train(epoch):
-    model.train()
-    for batch_id, (data, target) in enumerate(traindata):
-        data = data.cuda()
-        target = target.cuda()
-        optimizer.zero_grad()
-        out = model(data)
-        criterion = func.nll_loss
-        loss = criterion(out, target)
-        loss.backward()
-        optimizer.step()
-        print('Epoche: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-            epoch, batch_id*len(data), len(train.dataset), 100. * batch_id / len(traindata), loss.data[0]
-        ))
-        
+        net.train()
+        for i, (data, target) in enumerate(trainloader, 0):
+            data, target = data.cuda(), target.cuda()
+            optimizer.zero_grad()
+            outputs = net(data)
+            loss = criterion(outputs, target)
+            loss.backward()
+            optimizer.step()
+
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, i * len(data), len(trainloader.dataset),
+                       100. * i / len(trainloader), loss.item()))
+
+
+
+
+def test():
+    net.eval()
+    testloss = 0
+    correct = 0
+
+    for data, target in testloader:
+        data= data.cuda()
+        target= target.cuda()
+        outputs=net(data)
+        testloss += F.nll_loss(outputs, target, reduction='sum').item()
+        pred = outputs.argmax(dim=1, keepdim=True)
+        correct += pred.eq(target.view_as(pred)).cpu().sum().item()
+    loss= testloss/len(testloader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        loss, correct, len(testloader.dataset),
+        100. * correct / len(testloader.dataset)))
+
+
+
+
+
+for epoch in range(1, epochs):
+    train(epoch)
+    test()
